4ac5e12877daed2240358794be7ddb7c362aa3cc
mari0m0reira <mariomsjunior@gmail.com>
2024-05-16 23:30:54 -0300

    footer adicionado

--- ts/src/app/script.js
+++ ts/src/app/script.js
@@ -1,11 +1,9 @@
 function vermais(){
     let restoConteudo = document.getElementById("resto-conteudo");
-    let btnVermais = document.getElementById("btnVerMais");
+    
     let mais = document.getElementById("ver-mais");
 
-    restoConteudo.style.transitionDuration = "2s";
-    mais.style.transitionDuration = "2s";
-    btnVermais.style.transitionDuration = "2s";
+    let btnVermais = document.getElementsByClassName('btn-show-more');
   
     if(restoConteudo.style.display == "inline"){
         restoConteudo.style.display="none";
