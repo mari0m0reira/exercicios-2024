637e71be439b13e7dd197bd494b3e88614b407a3
mari0m0reira <mariomsjunior@gmail.com>
2024-05-20 16:20:50 -0300

    Ajuste do botão criar tópico

--- ts/src/app/script.js
+++ ts/src/app/script.js
@@ -14,36 +14,6 @@ function vermais(){
     }
 }
 
-function criarTopico(){
-    let compatilheIdeias = document.getElementById("compatilhe-suas-ideias");
-    let duvidasSugestoes = document.getElementById("duvidas-e-sugestoes");
-    let seuTopico = document.getElementById("seu-topico");
-
-    compatilheIdeias.style.display ="none";
-    duvidasSugestoes.style.display="inline";
-    seuTopico.style.display="none";
-}
-
-function enviarTopico(){
-    let compatilheIdeias = document.getElementById("compatilhe-suas-ideias");
-    let duvidasSugestoes = document.getElementById("duvidas-e-sugestoes");
-    let seuTopico = document.getElementById("seu-topico");
-
-    compatilheIdeias.style.display ="none";
-    duvidasSugestoes.style.display="none";
-    seuTopico.style.display="inline";
-}
-
-function criarNovoTopico(){
-    let compatilheIdeias = document.getElementById("compatilhe-suas-ideias");
-    let duvidasSugestoes = document.getElementById("duvidas-e-sugestoes");
-    let seuTopico = document.getElementById("seu-topico");
-
-    compatilheIdeias.style.display ="none";
-    duvidasSugestoes.style.display="inline";
-    seuTopico.style.display="none";
-}
-
 function verRespostas(){
     let respostas = document.getElementById("answers");
     let tresPontos = document.getElementById("tres=pontos");
@@ -59,3 +29,72 @@ function verRespostas(){
         tresPontos.style.display="inline";
     }
 }
+function criarTopico(){
+    let compatilheIdeias = document.getElementById("compatilhe-suas-ideias");
+    let duvidasSugestoes = document.getElementById("duvidas-e-sugestoes");
+    let seuTopico = document.getElementById("seu-topico");
+    let backGray = document.getElementById("back-gray");
+    let preenchimentoEnviar = document.getElementsByClassName('preenchimento-enviar');
+    let btnCreateTopic = document.getElementById("create-topic");
+    let formatacaoBotao = document.getElementById("formatacaoBotao");    
+    
+    let riscoEsquerda = document.getElementById("risco-esquerda");
+    let riscoDireita = document.getElementById("risco-direita");
+
+    
+    var style = document.createElement('style');
+
+    if(btnCreateTopic.textContent == 'Enviar'){                
+        btnCreateTopic.textContent = 'criar novo tópico';
+        btnCreateTopic.classList.add('btn-create-new-topic');
+
+        style.innerHTML = '.create-topic::before { content: none; }';
+        document.head.appendChild(style);
+
+        riscoDireita.style.display = "inline";
+        riscoEsquerda.classList.add('risco-horizontal');
+        riscoEsquerda.classList.remove('preenchimento-enviar');
+        riscoEsquerda.style.display = "inline";
+        
+        backGray.style.display ="inline";        
+        
+        seuTopico.style.display="inline";
+        compatilheIdeias.style.display ="none";
+        duvidasSugestoes.style.display ='none';
+
+    }else if(btnCreateTopic.textContent == 'criar tópico'){  
+        btnCreateTopic.textContent = 'Enviar';
+        formatacaoBotao.classList.add('enviar');        
+        btnCreateTopic.classList.add('btn-enviar');
+
+        style.innerHTML = '.create-topic::before { content: none; }';
+        document.head.appendChild(style);
+
+        riscoDireita.style.display = "none";
+        riscoEsquerda.classList.add('preenchimento-enviar');
+        riscoEsquerda.style.display = "inline";      
+        backGray.style.display ="none";
+        compatilheIdeias.style.display ="none";
+        duvidasSugestoes.style.display ="inline";
+
+    }else if(btnCreateTopic.textContent == 'criar novo tópico'){
+        btnCreateTopic.textContent = 'Enviar';        
+        btnCreateTopic.classList.remove('btn-create-new-topic');
+        formatacaoBotao.classList.add('enviar');
+        btnCreateTopic.classList.add('btn-enviar');
+        
+        style.innerHTML = '.create-topic::before { content: none; }';
+        document.head.appendChild(style);
+
+        riscoDireita.style.display = "none";
+        riscoEsquerda.classList.add('preenchimento-enviar');
+        riscoEsquerda.style.display = "inline";
+
+        backGray.style.display ="none";
+        
+        seuTopico.style.display="none";
+        compatilheIdeias.style.display ="none";
+        duvidasSugestoes.style.display ="inline";
+    }      
+}
+
